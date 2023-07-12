15251554ac0257c4dce2e0c5a80f1331c354da54
Evghenii <evghenii.sirbu@yahoo.com>
2023-07-12 23:27:32 +0300

    initial commit

---
+++ src/main/java/com/example/mediquiz/MediQuizApplication.java
@@ -0,0 +1,13 @@
+package com.example.mediquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediQuizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediQuizApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mediquiz/MediQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mediquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediQuizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
