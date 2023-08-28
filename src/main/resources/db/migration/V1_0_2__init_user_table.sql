8dde68e7c1527f879a78d8cf5d694f5335e086ab
Evghenii <evghenii.sirbu@yahoo.com>
2023-08-28 14:41:34 +0300

    added database init for user and user_role tables

---
+++ src/main/resources/db/migration/V1_0_2__init_user_table.sql
@@ -0,0 +1,3 @@
+INSERT INTO user(id, email, name, password)
+VALUES ('49a7e292-44a6-11ee-be56-0242ac120002', 'johndoy1@mail.ro', 'John Doy 1', '$2a$10$TkLE7zNUT8fdDo5.O9c1H.sLwJdSny081EGsolDea6xOzmULfdHdq'),
+       ('4e854cdc-44a6-11ee-be56-0242ac120002', 'johndoy2@mail.ro', 'John Doy 2', '$2a$12$hmhfC6z6NHlWYQagZAa4eusfJ53N..4EdMRmT5vHB61jhWbgzb8K6');
---
+++ src/main/resources/db/migration/V1_0_3__init_user_role_table.sql
@@ -0,0 +1,3 @@
+insert into user_roles (user_id, role_id)
+values  ('49a7e292-44a6-11ee-be56-0242ac120002','00314d0a-20fd-11ee-be56-0242ac120002'),
+        ('4e854cdc-44a6-11ee-be56-0242ac120002','ca69442a-20fc-11ee-be56-0242ac120002');
