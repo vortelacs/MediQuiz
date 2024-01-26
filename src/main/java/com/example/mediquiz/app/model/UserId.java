df22debf449eee2eeccc7e266cc247df9eed6490
Evghenii <evghenii.sirbu@yahoo.com>
2024-01-27 00:25:30 +0100

    Added userId model

---
+++ src/main/java/com/example/mediquiz/app/model/UserId.java
@@ -0,0 +1,14 @@
+package com.example.mediquiz.app.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserId {
+    private String userId;
+}
